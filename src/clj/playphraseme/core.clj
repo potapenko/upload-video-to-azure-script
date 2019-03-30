e6aa0ed28f18d052cd8c9022d27a28a43ab298e8
Eugene Potapenko <eugene.john.potapenko@gmail.com>
2019-03-30 16:40:15 +0100

    fix

--- src/clj/playphraseme/core.clj
+++ src/clj/playphraseme/core.clj
@@ -1,11 +1,13 @@
 (ns playphraseme.core
   (:gen-class)
   (:require [taoensso.timbre :as log]
-            [mount.core :as mount]))
+            [mount.core :as mount]
+            [playphraseme.storage :as storage]))
 
 (defn start-app [args]
   (log/info "Uploader started!")
-  (mount/start))
+  (mount/start)
+  (storage/start))
 
 (defn -main [& args]
   (start-app args))
