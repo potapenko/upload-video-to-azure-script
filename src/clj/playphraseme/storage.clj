fa63f1590b7416e10c094764fe4b15897a03540e
Eugene Potapenko <eugene.john.potapenko@gmail.com>
2019-03-30 14:55:15 +0100

    next

--- src/clj/playphraseme/storage.clj
+++ src/clj/playphraseme/storage.clj
@@ -1,19 +1,51 @@
 (ns playphraseme.storage
   (:refer-clojure :exclude [list])
-  (:require [playphraseme.azure-blob :as azure]))
+  (:require [playphraseme.azure-blob :as azure]
+            [playphraseme.config :refer [env]]
+            [playphraseme.queries.phrases :as phrases]
+            [mount.core :as mount]))
 
 (def cnt (azure/blob-container
           {:storage-account-name "playphraseme8video"
            :storage-account-key  "vZm7fDaoW5LSgGduwCg/+9Z0jqvPJF1Ct1gO0HxVJtvfHPE6LoQVSZrAZV9HvtNQ5O69uxl8YI9cBGlw9TCOlg=="
            :storage-container    "media"}))
 
-(defn upload-phrase-video [{:keys [id movie]}]
-  )
+(defn mark-phrase-uploaded [phrase]
+  (phrases/update-phrase!
+   (assoc phrase :state 1)))
+
+(defn mark-not-have-video [phrase]
+  (phrases/update-phrase!
+   (assoc phrase :have-video false)))
+
+(defn upload-phrase-video [{:keys [id movie] :as phrase}]
+  (let [{:keys [phrases-dir]} env
+        path                  (format "%s/%s.mp4" movie id)
+        res                   (azure/upload-blob
+                               cnt path (str phrases-dir "/" path))]
+    (println "uploaded video file:" path "->" res)
+    (when (= :ok res)
+      (mark-phrase-uploaded phrase))))
+
+(defn sync-video []
+  (let [{:keys [parallel]} env]
+   (loop []
+     (let [phrases (phrases/find-phrases {:have-video true
+                                          :state      nil} 0 10)]
+       (when-not (empty? phrases)
+         (->> phrases
+              (partition-all parallel)
+              (map (fn [part]
+                     (->> part (map upload-phrase-video)))))
+         #_(recur))))))
 
 (comment
+  (mount/start)
+
+  (sync-video)
+
 
   (azure/list cnt)
 
-  (azure/upload-blob cnt "dir/dir/AnyFile.txt" "README.md")
 
   )
